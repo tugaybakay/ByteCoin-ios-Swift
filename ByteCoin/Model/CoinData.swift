4ccd742d084b7f380e10b72542bd807b20f47eb1
tugaybakay <108657479+tugaybakay@users.noreply.github.com>
2023-08-17 09:56:28 +0300

    Update CoinData.swift

--- ByteCoin/Model/CoinData.swift
+++ ByteCoin/Model/CoinData.swift
@@ -1,11 +1,3 @@
-//
-//  CoinData.swift
-//  ByteCoin
-//
-//  Created by MacOS on 6.08.2023.
-//  Copyright © 2023 The App Brewery. All rights reserved.
-//
-
 import Foundation
 
 struct CoinData: Decodable{
